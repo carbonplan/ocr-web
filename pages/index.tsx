dd2ce8f4a9ceb976a909f472957684bcb5308764
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-03-31 15:29:31 -0500

    sat toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { Box, Container } from 'theme-ui'
 //@ts-ignore
-import { Header, Dimmer } from '@carbonplan/components'
+import { Header, Dimmer, Tag } from '@carbonplan/components'
 //@ts-ignore
 import { Sidebar } from '@carbonplan/layouts'
 import { Map, Geocode } from '../components'
-
+import { useLocationStore } from '../store/location'
 const Index = () => {
+  const { satellite, setSatellite } = useLocationStore()
   return (
     <>
       <Container>
@@ -23,7 +24,15 @@ const Index = () => {
       >
         <Sidebar expanded={true} side='left' width={4}>
           <Geocode />
-          <Dimmer />
+          <Tag
+            label='Satellite'
+            value={satellite}
+            onClick={() => setSatellite(!satellite)}
+          >
+            Satellite
+          </Tag>
+
+          <Dimmer sx={{ position: 'absolute', bottom: 2, left: 2 }} />
         </Sidebar>
         <Map />
       </Box>
--- store/location.ts
+++ store/location.ts
@@ -4,9 +4,13 @@ import { Location } from '../types/location'
 type LocationStore = {
   selectedLocation: Location | null
   setSelectedLocation: (location: Location | null) => void
+  satellite: boolean
+  setSatellite: (satellite: boolean) => void
 }
 
 export const useLocationStore = create<LocationStore>((set) => ({
   selectedLocation: null,
   setSelectedLocation: (location) => set({ selectedLocation: location }),
+  satellite: false,
+  setSatellite: (satellite) => set({ satellite }),
 }))
