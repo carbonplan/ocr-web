b57fb3321f03e97d33bc0633fb3d52b9705883e9
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-06-16 16:54:39 -0500

    revamp config

--- lib/config.ts
+++ lib/config.ts
@@ -1,30 +1,28 @@
-export const DATA_SOURCES = {
-  buildings:
-    'https://carbonplan-ocr.s3.us-west-2.amazonaws.com/intermediate/fire-risk/vector/wind_layer_and_RPS.pmtiles',
-} as const
-
-export const LAYER_NAMES = {
-  buildings: 'risk',
-} as const
-
-export const RISK_ATTRIBUTES = {
-  baseRisk: 'USFS_RPS',
-  windRisk: 'wind_risk',
-} as const
-
-export const RISK_BOUNDS = {
-  min: 0,
-  max: 0.001,
-  get mid() {
-    return (this.max - this.min) / 2 + this.min
+export const LAYERS = {
+  buildings: {
+    dataSource:
+      'https://carbonplan-ocr.s3.us-west-2.amazonaws.com/intermediate/fire-risk/vector/wind_layer_and_RPS_CA_region.pmtiles',
+    layerName: 'risk',
+    sourceId: 'buildings',
+    layerIds: {
+      fill: 'buildings-fill',
+      line: 'buildings-line',
+    },
   },
 } as const
 
-export const MAP_LAYER_IDS = {
-  buildingsFill: 'buildings-fill',
-  buildingsLine: 'buildings-line',
-} as const
-
-export const MAP_SOURCE_IDS = {
-  buildings: 'buildings',
+export const RISKS = {
+  fire: {
+    attributes: {
+      baseRisk: 'USFS_RPS',
+      windRisk: 'wind_risk',
+    },
+    bounds: {
+      min: 0,
+      max: 0.001,
+      get mid() {
+        return (this.max - this.min) / 2 + this.min
+      },
+    },
+  },
 } as const
