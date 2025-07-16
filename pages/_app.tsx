ae4819ba7330fe4e655896f31511379c8e8d888d
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-07-16 18:17:22 -0500

    reorder imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
+import { useEffect } from 'react'
 import Script from 'next/script'
-import { ThemeProvider } from 'theme-ui'
-import '@carbonplan/components/fonts.css'
-import '@carbonplan/components/globals.css'
-import theme from '@/lib/theme'
 import type { AppProps } from 'next/app'
 // @ts-expect-error - carbonplan auth types not available
 import { AuthProvider } from '@carbonplan/auth'
+import { ThemeProvider } from 'theme-ui'
+import theme from '@/lib/theme'
 import { useLocationStore } from '@/store/location'
+import '@carbonplan/components/fonts.css'
+import '@carbonplan/components/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   const toggleAdvancedMode = useLocationStore(
