5ae8e472936ab6e9962001bb6e49f07a6960f2f4
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-08-26 15:50:59 -0500

    USFS_RPS

--- lib/config.ts
+++ lib/config.ts
@@ -29,8 +29,8 @@ export const LAYERS = {
 
 const FIRE_ATTRIBUTES: FireRisk<string> = {
   baseRisk: {
-    current: 'risk_2011',
-    future: 'risk_2047',
+    current: 'USFS_RPS',
+    future: 'USFS_RPS',
   },
   windRisk: {
     current: 'wind_risk_2011',
