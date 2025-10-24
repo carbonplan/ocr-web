6614bad5599f258acb8452939633a2cb45779742
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-10-24 12:36:46 -0500

    pass filename to download service

--- components/results/download.tsx
+++ components/results/download.tsx
@@ -79,6 +79,7 @@ export const Download = ({ disabled, geography }: DownloadProps) => {
           data_format: format,
           geoid: geoid,
           region_type: REGION_TYPES[geography],
+          file_name: `${filename}.${format}`,
         }),
       })
       const payload = await res.json()
