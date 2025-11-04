9964f7e9d31312b533863bf6a98304f9440cbe74
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-11-04 08:45:44 -0500

    clear selections immediately on building click

--- components/buildings.tsx
+++ components/buildings.tsx
@@ -205,6 +205,7 @@ const Buildings = () => {
   const handleMapClick = useCallback(
     async (e: MapMouseEvent) => {
       if (!map) return
+      clearSelections()
 
       const features = map.queryRenderedFeatures(e.point, {
         layers: [LAYERS.buildings.layerIds.fill],
