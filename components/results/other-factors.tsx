786be249b512f4c15d1dfa94eb0f0eb19831f067
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-11-06 11:07:38 -0600

    add factor resources

--- components/results/other-factors.tsx
+++ components/results/other-factors.tsx
@@ -1,9 +1,100 @@
-import { Box } from 'theme-ui'
+import { Box, Link } from 'theme-ui'
 //@ts-expect-error - carbonplan components types not available
 import { Table } from '@carbonplan/components'
 import TooltipWrapper from '../tooltip'
 
+const factors = [
+  {
+    name: 'Fire-resistant construction and maintenance',
+    impact: 'Lower',
+    resources: [
+      'https://wildfirerisk.org/reduce-risk/ignition-resistant-homes/',
+      'https://www.nfpa.org/education-and-research/wildfire/preparing-homes-for-wildfire',
+      'https://cpaw.headwaterseconomics.org/wp-content/uploads/2022/08/2022HE-OwnYourZoneHouse-R3_CPAW.pdf',
+    ],
+  },
+  {
+    name: 'Community mitigation',
+    impact: 'Lower',
+    resources: [
+      'https://www.nfpa.org/education-and-research/policy-and-action/outthink-wildfire',
+      'https://www.iafc.org/docs/default-source/pdf/wild_cwppleadrsguide.pdf',
+      'https://fireadaptednetwork.org/resources/fac-assessment-tool/',
+      'https://wildfirerisk.org/reduce-risk/land-use-planning/',
+    ],
+  },
+  {
+    name: 'Community emergency response',
+    impact: 'Lower',
+    resources: [
+      'https://wildfirerisk.org/reduce-risk/evacuation-readiness/',
+      'https://wildfirerisk.org/reduce-risk/wildfire-response',
+    ],
+  },
+  {
+    name: 'Fires in the last ~5 years',
+    impact: 'Lower',
+    resources: [
+      'https://www.fs.usda.gov/managing-land/prescribed-fire',
+      'https://cires.colorado.edu/news/fewer-forest-fires-burn-north-america-today-past-and-thats-bad-thing',
+    ],
+  },
+  {
+    name: 'Access limitations',
+    impact: 'Higher',
+    resources: [],
+  },
+  {
+    name: 'Value of resources at risk',
+    impact: 'Higher',
+    resources: ['https://hazards.fema.gov/nri/expected-annual-loss'],
+  },
+  {
+    name: 'Ignition patterns',
+    impact: 'Either',
+    resources: [
+      'https://wildfirerisk.org/reduce-risk/prevent-ignitions/',
+      'https://dnr.wa.gov/wildfire-resources/wildfire-prevention',
+      'https://smokeybear.com',
+    ],
+  },
+]
+
 const OtherFactors = () => {
+  const tableData = [
+    ['Factor', 'Risk impact'],
+    ...factors.map((factor) => {
+      const factorCell = (
+        <>
+          <Box>{factor.name}</Box>
+          {factor.resources.length > 0 && (
+            <Box sx={{ fontSize: 0, color: 'secondary' }}>
+              Resources:{' '}
+              {factor.resources.map((url, index) => (
+                <Box key={url} as='span'>
+                  <Link
+                    href={url}
+                    target='_blank'
+                    sx={{
+                      color: 'secondary',
+                      textDecoration: 'none',
+                      '&:hover': {
+                        color: 'primary',
+                      },
+                    }}
+                  >
+                    [{index + 1}]{' '}
+                  </Link>
+                </Box>
+              ))}
+            </Box>
+          )}
+        </>
+      )
+      return [factorCell, factor.impact]
+    }),
+  ]
+
   return (
     <Box sx={{ mt: '20px' }}>
       <TooltipWrapper
@@ -19,13 +110,7 @@ const OtherFactors = () => {
         columns={3}
         start={[1, 3]}
         width={[2, 1]}
-        data={[
-          ['Factor', 'Risk impact'],
-          ['Building retrofit', 'Lower'],
-          ['Community emergency response', 'Lower'],
-          ['Previous fire', 'Lower'],
-          ['Access limitations', 'Higher'],
-        ]}
+        data={tableData}
         index={false}
         borderTop={false}
         sx={{
