eb8e81d9250835c3152bf7e6eb10dc85207833fa
Kata Martin <katasm@gmail.com>
2025-11-07 12:13:21 -0500

    Update intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -19,10 +19,13 @@ const Intro = () => {
       </Box>
       <Box sx={{ mb: 3, variant: 'description' }}>
         This explorer shows fire risk across the continental US. Search for an
-        address or use the map to explore risk data. More risks will be added in
-        the future. Read our <Link href='#TK'>methods</Link>, the{' '}
+        address or use the map to explore risk data. More hazards will be added
+        in the future. Read our <Link href='#TK'>methods</Link>, the{' '}
         <Link href='#TK'>explainer</Link>, or{' '}
-        <Link href='#TK'>download the data</Link> for more details.
+        <Link href='https://carbonplan.github.io/ocr/reference/data-downloads/'>
+          download the data
+        </Link>{' '}
+        for more details.
         <Box sx={{ mt: 3, color: 'secondary', fontSize: 1 }}>
           By viewing Open Climate Risk, you agree to CarbonPlan’s{' '}
           <Link sx={{ color: 'secondary' }} href='https://carbonplan.org/terms'>
