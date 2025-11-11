ede5da9bb4ac0c5febe6811ddc86e3a6dcb919ce
Kata Martin <katasm@gmail.com>
2025-11-11 15:40:48 -0500

    Specify custom src and data-api
    Consistent with Script-based approach

--- hocs/with-auth-and-plausible.tsx
+++ hocs/with-auth-and-plausible.tsx
@@ -24,6 +24,11 @@ export function withAuthAndPlausible<P extends object>(
         <PlausibleProvider
           domain='carbonplan.org'
           customDomain='https://carbonplan.org'
+          scriptProps={{
+            src: 'https://carbonplan.org/js/script.file-downloads.outbound-links.js',
+            // @ts-expect-error - following example https://github.com/4lejandrito/next-plausible/blob/8aa2279a9c2c8d03508c198abc4b219591efbc5c/test/page/pages/scriptProps.js
+            'data-api': 'https://carbonplan.org/proxy/api/event',
+          }}
           enabled={process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'}
           trackOutboundLinks
           trackFileDownloads
